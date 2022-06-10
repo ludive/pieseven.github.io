f2c5cda73325de42bb09315ae82332c3c6ef2716
PIESEVEN <98004908+pieseven-works@users.noreply.github.com>
2022-06-10 13:56:12 +0800

    feature: Viewerjs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 // main.js
 
-// Initial.
 function initial() {
     console.log("Citrus - The Theme of Jekyll.\nDesigned by Harvey Wu.\n");
     console.log("Theme License:\n\tGPL-3.0 License\t\t(https://www.gnu.org/licenses/gpl-3.0.html)\n\tCC BY-NC-SA 4.0\t\t(https://creativecommons.org/licenses/by-nc-sa/4.0/)")
